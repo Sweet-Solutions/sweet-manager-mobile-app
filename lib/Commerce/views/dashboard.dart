bbf0ee0cbff1fb27b3a010e1e93cb747f5babc06
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-10 16:32:25 -0500

    feat(commerce): added dashboard setup.

---
+++ lib/Commerce/views/dashboard.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatefulWidget{
+  
+  @override
+  State<StatefulWidget> createState() => DashboardState();
+}
+
+class DashboardState extends State<DashboardScreen>{
+  @override
+  Widget build(BuildContext context) {
+  
+    return const Center(
+      child: Text('Not yet implemented'),
+    );
+  }
+}
