aa35b0a04fe1ed07755566171e99d7bacb5e16e8
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-10 15:21:27 -0500

    feat: added screen for authentication status.

---
+++ lib/IAM/views/splash_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:sweetmanager/IAM/services/auth_service.dart';
+
+// In the following line you configure or declare that SplashScreen is a Widget.
+class SplashScreen extends StatelessWidget{
+  final AuthService _authService = AuthService();
+  
+  // Configure the Authentication Status (isAuthenticated?)
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder(
+        future: _authService.isAuthenticated(),
+        builder: (context, AsyncSnapshot<bool> snapshot){
+          if(snapshot.connectionState == ConnectionState.waiting)
+          {
+            return const Center(
+              child: CircularProgressIndicator()
+            );
+          }
+          else
+          {
+            WidgetsBinding.instance.addPostFrameCallback((_){
+              if(snapshot.hasData && snapshot.data == true)
+              {
+                Navigator.pushReplacementNamed(context, '/home');
+              }
+              else
+              {
+                Navigator.pushReplacementNamed(context, '/login');
+              }
+            });
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        ),
+    );
+  }
+
+}
