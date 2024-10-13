03a8bd24fcba7e52f28edf8de04473a8e8282d68
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-13 12:37:57 -0500

    feat(iam): added default main route.

--- lib/IAM/views/home.dart
+++ lib/IAM/views/home.dart
@@ -1,11 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:sweetmanager/IAM/views/login.dart';
 
 class HomeView extends StatelessWidget{
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/hotel_background.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Sweet Manager',
+                style: TextStyle(
+                  fontSize: 28,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 10),
+              const Text(
+                'Optimiza la gestión de tu hotel y ofrece experiencias excepcionales a tus huéspedes!',
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueAccent,
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => LogInScreen()),
+                  );
+                },
+                child: const Text(
+                  'Get Started',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 
