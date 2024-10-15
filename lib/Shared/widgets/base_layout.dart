5e8e37ef6b74e3720c8e5435c7a3740f6b8c1232
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-15 17:58:55 -0500

    chore(iam): updated base layout.

--- lib/Shared/widgets/base_layout.dart
+++ lib/Shared/widgets/base_layout.dart
@@ -4,79 +4,90 @@ import 'package:flutter/material.dart';
 
 class BaseLayout extends StatelessWidget{
   
-  final Widget child; // The content of the screen
+  final Widget childScreen; // The content of the screen
 
   final String role; // User's role to define sidebar's lit
 
-  BaseLayout({required this.role, required this.child});
+  BaseLayout({required this.role, required this.childScreen});
 
   @override
   Widget build(BuildContext context) {
     
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sweet Manager'),
-        leading: Icon(Icons.home),
+        title: const Text('Sweet Manager'),
+        backgroundColor: Colors.blue,
       ),
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
-          children: [],
+          children: _getSidebarOptions(context),
         ),
       ),
-      body: child,
+      body: childScreen,
     );
   }
 
   List<Widget> _getSidebarOptions(BuildContext context){
+    if(role == '')
+    {
+      return [
+        ListTile(
+          leading: const Icon(Icons.not_accessible),
+          title: const Text('No current Routes for now'),
+          subtitle: const Text('Login First :)'),
+          onTap: (){ },
+        )
+      ];
+    }
     if(role == 'ROLE_OWNER')
     {
       return [
         ListTile(
-          leading: Icon(Icons.dashboard),
-          title: Text('Dashboard'),
+          leading: const Icon(Icons.dashboard),
+          title: const Text('Dashboard'),
           onTap: (){
             Navigator.pushNamed(context, '/dashboard');
           },
         ),
         ListTile(
-          leading: Icon(Icons.message),
-          title: Text('Notifications'),
+          leading: const Icon(Icons.message),
+          title: const Text('Notifications'),
           onTap: (){
             Navigator.pushNamed(context, '/notifications');
           },
         ),
         ListTile(
-          leading: Icon(Icons.subscriptions),
-          title: Text('Current Subscription'),
+          leading: const Icon(Icons.subscriptions),
+          title: const Text('Current Subscription'),
           onTap: (){
             Navigator.pushNamed(context, '/current_subscription');
           },
         ),
         ListTile(
-          leading: Icon(Icons.emoji_transportation),
-          title: Text('Suppliers Management'),
+          leading: const Icon(Icons.emoji_transportation),
+          title: const Text('Suppliers Management'),
           onTap: () {
             Navigator.pushNamed(context, '/providers');
           },
         ),
         ListTile(
-          leading: Icon(Icons.food_bank),
-          title: Text('Supplies Management'),
+          leading: const Icon(Icons.food_bank),
+          title: const Text('Supplies Management'),
           onTap: (){
             Navigator.pushNamed(context, '/supplies');
           },
         ),
         ListTile(
-          leading: Icon(Icons.room),
-          title: Text('Rooms Management'),
+          leading: const Icon(Icons.room),
+          title: const Text('Rooms Management'),
           onTap: (){
             Navigator.pushNamed(context, '/rooms');
           },
         ),
         ListTile(
-          leading: Icon(Icons.person),
-          title: Text('Profiles'),
+          leading: const Icon(Icons.person),
+          title: const Text('Profiles'),
           onTap: (){
             Navigator.pushNamed(context, '/profile');
           },
@@ -93,50 +104,50 @@ class BaseLayout extends StatelessWidget{
     {
       return [
         ListTile(
-          leading: Icon(Icons.dashboard),
-          title: Text('Dashboard'),
+          leading: const Icon(Icons.dashboard),
+          title: const Text('Dashboard'),
           onTap: (){
             Navigator.pushNamed(context, '/dashboard');
           },
         ),
         ListTile(
-          leading: Icon(Icons.emoji_transportation),
-          title: Text('Suppliers Management'),
+          leading: const Icon(Icons.emoji_transportation),
+          title: const Text('Suppliers Management'),
           onTap: () {
             Navigator.pushNamed(context, '/providers');
           },
         ),
         ListTile(
-          leading: Icon(Icons.food_bank),
-          title: Text('Supplies Management'),
+          leading: const Icon(Icons.food_bank),
+          title: const Text('Supplies Management'),
           onTap: (){
             Navigator.pushNamed(context, '/supplies');
           },
         ),
         ListTile(
-          leading: Icon(Icons.room),
-          title: Text('Rooms Management'),
+          leading: const Icon(Icons.room),
+          title: const Text('Rooms Management'),
           onTap: (){
             Navigator.pushNamed(context, '/rooms');
           },
         ),
         ListTile(
-          leading: Icon(Icons.message),
-          title: Text('Messages'),
+          leading: const Icon(Icons.message),
+          title: const Text('Messages'),
           onTap: (){
             Navigator.pushNamed(context, '/messages');
           },
         ),
         ListTile(
-          leading: Icon(Icons.report),
-          title: Text('Reports'),
+          leading: const Icon(Icons.report),
+          title: const Text('Reports'),
           onTap: (){
             Navigator.pushNamed(context, '/reports');
           },
         ),
         ListTile(
-          leading: Icon(Icons.person),
-          title: Text('Profiles'),
+          leading: const Icon(Icons.person),
+          title: const Text('Profiles'),
           onTap: (){
             Navigator.pushNamed(context, '/profile');
           },
