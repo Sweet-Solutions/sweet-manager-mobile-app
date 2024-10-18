f72faa92116a96adba990026b46a3b31efef9933
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-18 18:58:02 -0500

    feat(iam): added payment owner model.

---
+++ lib/Commerce/models/payment_owner.dart
@@ -0,0 +1,14 @@
+class PaymentOwner
+{
+  final int id;
+
+  final int ownersId;
+
+  final String description;
+
+  final double finalAmount;
+
+
+  PaymentOwner({required this.id,required this.ownersId, required this.description, required this.finalAmount});
+
+}
