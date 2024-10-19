fe777014e4e340be2f8d921da72849b06f301c87
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-18 19:18:22 -0500

    feat(iam): added owner model.

---
+++ lib/IAM/models/owner_entity.dart
@@ -0,0 +1,15 @@
+class Owner
+{
+  final int id;
+
+  final String name;
+
+  final String surname;
+
+  final int phone;
+
+  final String email;
+
+  Owner({required this.id, required this.name, required this.surname, required this.phone, required this.email});
+
+}
