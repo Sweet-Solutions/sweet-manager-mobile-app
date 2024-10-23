018d309d1990ef3091f152fddc3881a8b382df96
aaron acuña <aaronacu_85@hotmail.com>
2024-10-22 20:45:28 -0500

    feat(monitoring): added logic for request information of the web service.

--- lib/Monitoring/views/tableroom.dart
+++ lib/Monitoring/views/tableroom.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 
+import '../models/room.dart';
 import '../services/roomservice.dart';
 
 class TableRoom extends StatelessWidget {
+
+  const TableRoom({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -40,7 +43,7 @@ class TableRoom extends StatelessWidget {
                   DataColumn(label: Text('Estado')),
                   DataColumn(label: Text('Opciones'))
                 ],
-                source: RoomService(context),
+                source: DataTableRoom(context),
               ),
             ),
           ],
@@ -48,4 +51,60 @@ class TableRoom extends StatelessWidget {
       ),
     );
   }
+}
+
+class DataTableRoom extends DataTableSource {
+
+  final BuildContext context;
+
+  DataTableRoom(this.context);
+
+  final List<Room> _data = [
+    Room(id: 1, typeRoomId: 2, hotelId: 101, roomState: 'OCUPADO'),
+    Room(id: 2, typeRoomId: 1, hotelId: 102, roomState: 'LIBRE'),
+  ];
+
+  @override
+  DataRow getRow(int index) {
+
+    final data = _data[index];
+
+    return DataRow(cells: [
+
+      DataCell(Text(data.id.toString())),
+      DataCell(Text(data.typeRoomId.toString())),
+      DataCell(Text(data.hotelId.toString())),
+      DataCell(Text(data.roomState.toString())),
+      DataCell(TextButton(
+        onPressed: () {
+          final id = data.id;
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text("Editar habitación"),
+                content: Text("Se modificara la información de la habitación $id"),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text("Aceptar"),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        child: const Text('Modificar'),
+      )),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+  @override
+  int get rowCount => _data.length;
+  @override
+  int get selectedRowCount => 0;
 }
