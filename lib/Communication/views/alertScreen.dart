2d19fa4c7a4a581bc9674490b84c638dac48c0c0
Mauricio <mauriciorojasanchez@gmail.com>
2024-10-24 15:07:32 -0500

    chore: deleted unused fucntions.

--- lib/Communication/views/alertScreen.dart
+++ lib/Communication/views/alertScreen.dart
@@ -1,23 +1,12 @@
-
 import 'package:flutter/material.dart';
 import 'package:sweetmanager/Communication/services/NotificationService.dart';
 import '../models/notification.dart';
-import 'package:sweetmanager/IAM/services/auth_service.dart'; // Import AuthService for token management
 import 'package:sweetmanager/Shared/widgets/base_layout.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
 
 class AlertsScreen extends StatefulWidget {
   const AlertsScreen({Key? key}) : super(key: key);
-
-@override
-Widget build(BuildContext context) {
-  return const BaseLayout(role: '', childScreen: AlertsScreen());
-}
-
-class AlertsScreen extends StatefulWidget {
-
-  const AlertsScreen({super.key});
 
   @override
   _AlertsScreenState createState() => _AlertsScreenState();
@@ -38,7 +27,7 @@ class _AlertsScreenState extends State<AlertsScreen> {
     fetchAlertNotifications(); 
   }
 
-  // Método para obtener el rol del token
+  // MÃ©todo para obtener el rol del token
   Future<String?> _getRole() async {
     String? token = await storage.read(key: 'token');
     if (token != null) {
@@ -64,20 +53,6 @@ class _AlertsScreenState extends State<AlertsScreen> {
       );
     }
   }
-  final List<Notifications> alertNotifications = [
-    Notifications(1, 1, 2, 3, 'Power Outage Scheduled', 'There will be a power outage in the hotel from 2 AM to...'),
-    Notifications(2, 1, 2, 3, 'Emergency Fire Drill', 'An emergency fire drill will take place at 10 AM today...'),
-    Notifications(3, 1, 2, 3, 'Water Supply Disruption', 'Several guests reported unusual behavior around the...'),
-    Notifications(1, 1, 2, 3, 'Low Stock of Guest Amenities', 'There will be a temporary disruption in the water supply...'),
-    Notifications(2, 1, 2, 3, 'Security System Update', 'A security system update is scheduled for tonight...'),
-    Notifications(1, 1, 2, 3, 'Lost Guest Property', 'A guest has reported a lost item in the lobby. Please...'),
-    Notifications(2, 1, 2, 3, 'Wi-Fi Outage', 'There is an ongoing Wi-Fi outage affecting the entire...'),
-    Notifications(4, 1, 2, 3, 'Broken Elevator', 'The elevator in the north wing is out of service. Please...'),
-    Notifications(1, 1, 2, 3, 'Unauthorized Access in Staff Area', 'We are experiencing a shortage of essential supplies...'),
-    Notifications(1, 1, 2, 3, 'HVAC System Failure', 'The HVAC system in the east wing is not functioning...'),
-  ];
-
-  final Map<int, bool> _isHoveringNotification = {};
 
   void removeNotification(int index) {
     setState(() {
@@ -187,6 +162,3 @@ class NotificationCard extends StatelessWidget {
     );
   }
 }
-
-
-
