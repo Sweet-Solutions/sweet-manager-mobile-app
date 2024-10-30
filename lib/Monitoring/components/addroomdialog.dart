6e98d492a312da953d4731deb59b7848f591a116
aaron acuña <aaronacu_85@hotmail.com>
2024-10-29 19:17:20 -0500

    feat(monitoring): added dialog for register new room.

---
+++ lib/Monitoring/components/addroomdialog.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:sweetmanager/Monitoring/models/room.dart';
+import 'package:sweetmanager/Monitoring/services/roomservice.dart';
+
+class AddRoomDialog extends StatefulWidget {
+  const AddRoomDialog({super.key});
+
+  @override
+  _AddRoomDialogState createState() => _AddRoomDialogState();
+}
+
+class _AddRoomDialogState extends State<AddRoomDialog> {
+
+  late RoomService roomService = RoomService();
+  late TextEditingController typeRoomId;
+  late TextEditingController hotelId;
+  late TextEditingController roomState;
+
+  @override
+  void initState() {
+    super.initState();
+    typeRoomId = TextEditingController();
+    hotelId = TextEditingController();
+    roomState = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text("Add new room"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            controller: typeRoomId,
+            decoration: const InputDecoration(labelText: 'Type Room ID'),
+            keyboardType: TextInputType.number,
+          ),
+          TextFormField(
+            controller: hotelId,
+            decoration: const InputDecoration(labelText: 'Hotel ID'),
+            keyboardType: TextInputType.number,
+          ),
+          TextFormField(
+            controller: roomState,
+            decoration: const InputDecoration(labelText: 'Room State'),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        TextButton(
+          child: const Text("Cancel"),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        TextButton(
+          child: const Text("Accept"),
+          onPressed: () {
+
+            final int newTypeRoomId = int.parse(typeRoomId.text);
+            final int newHotelId = int.parse(hotelId.text);
+            final String newRoomState = roomState.text;
+
+            roomService.createRoom(
+              Room(
+                id: 0,
+                typeRoomId: newTypeRoomId,
+                hotelId: newHotelId,
+                roomState: newRoomState,
+              ),
+            );
+
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    typeRoomId.dispose();
+    hotelId.dispose();
+    roomState.dispose();
+    super.dispose();
+  }
+}
