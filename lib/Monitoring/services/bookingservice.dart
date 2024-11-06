7edbc51856fb894fdd8f1511800611ebb01b653f
aaron acuña <aaronacu_85@hotmail.com>
2024-11-05 22:50:10 -0500

    fix(monitoring): corrected booking service for send token.

--- lib/Monitoring/services/bookingservice.dart
+++ lib/Monitoring/services/bookingservice.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:sweetmanager/Monitoring/models/booking.dart';
@@ -6,11 +7,18 @@ class BookingService {
 
   final String baseUrl = 'https://sweetmanager-api.ryzeon.me/api/bookings/';
 
+  final storage = const FlutterSecureStorage();
+
   Future<bool> createBooking(Booking booking) async {
+
+    final token = await storage.read(key: 'token');
 
     final response = await http.post(
       Uri.parse('${baseUrl}create-booking'),
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
       body: json.encode({
         'paymentCustomerId': booking.paymentCustomerId,
         'roomId': booking.roomId,
@@ -22,7 +30,7 @@ class BookingService {
       }),
     );
 
-    if (response.statusCode == 201) {
+    if (response.statusCode >= 200 && response.statusCode < 300) {
       return true;
     } else {
       throw Exception('Error ${response.statusCode}: ${response.body}');
@@ -31,16 +39,21 @@ class BookingService {
 
   Future<bool> updateBooking(int id, Booking booking) async {
 
+    final token = await storage.read(key: 'token');
+
     final response = await http.put(
       Uri.parse('${baseUrl}update-booking?id=$id'),
-      headers: {'Content-Type': 'application/json'},
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      },
       body: json.encode({
         'id': booking.id,
         'bookingState': booking.bookingState
       }),
     );
 
-    if (response.statusCode == 200) {
+    if (response.statusCode >= 200 && response.statusCode < 300) {
       return true;
     } else {
       throw Exception('Error ${response.statusCode}: ${response.body}');
@@ -49,10 +62,17 @@ class BookingService {
 
   Future<List<Booking>> getBookingsByHotelId(int hotelId) async {
 
-    final response = await http.get
-      (Uri.parse('${baseUrl}get-all-bookings/$hotelId'));
+    final token = await storage.read(key: 'token');
 
-    if (response.statusCode == 200) {
+    final response = await http.get(
+      Uri.parse('${baseUrl}get-all-bookings/$hotelId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      }
+    );
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
       List<dynamic> data = json.decode(response.body);
       return data.map((bookingJson) => Booking(
         id: bookingJson['id'],
@@ -73,14 +93,21 @@ class BookingService {
 
   Future<Booking> getBookingById(int id) async {
 
+    final token = await storage.read(key: 'token');
+
     if (id == 0) {
       throw Exception('The booking id cannot be 0.');
     }
 
-    final response = await http.get
-      (Uri.parse('${baseUrl}get-booking-by-id?id=$id'));
+    final response = await http.get(
+      Uri.parse('${baseUrl}get-booking-by-id?id=$id'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      }
+    );
 
-    if (response.statusCode == 200) {
+    if (response.statusCode >= 200 && response.statusCode < 300) {
       var bookingJson = json.decode(response.body);
       return Booking(
         id: bookingJson['id'],
