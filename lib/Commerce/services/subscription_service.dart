905ad8d2b9e0d1e71df67eb53f95640ab17a7434
Mauricio <mauriciorojasanchez@gmail.com>
2024-11-08 18:02:42 -0500

    feat(commerce): added subscription service.

---
+++ lib/Commerce/services/subscription_service.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import 'package:http/http.dart' as http;
+import 'package:sweetmanager/Commerce/models/contract_owners.dart';
+
+class SubscriptionService {
+  final baseUrl = 'https://sweetmanager-api.ryzeon.me';
+
+  final storage = const FlutterSecureStorage();
+
+  Future<ContractOwners?> fetchContractByOwnerId(int ownerId) async
+  {
+    try {
+      var token = await storage.read(key: 'token');
+
+      final response = await http.get(Uri.parse('$baseUrl/get-contract-by-owner-id?ownerId=$ownerId'),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token'
+      });
+  
+      if(response.statusCode == 200)
+      {
+        var jsonResponse = jsonDecode(response.body);
+
+        return ContractOwners(id: jsonResponse['id'], subscriptionId: jsonResponse['subscriptionId'], ownersId: jsonResponse['ownersId'],
+         startDate: jsonResponse['startDate'], finalDate: jsonResponse['endDate'], status: jsonResponse['state']);
+      }
+      else
+      {
+        return null;
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+
+}
