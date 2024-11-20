75797e7d3a3ea7b5263a1643848e5fc82da6a60d
Mauricio <mauriciorojasanchez@gmail.com>
2024-11-20 02:36:27 -0500

    fix(commerce): fixed factory object for hotel.

--- lib/Profiles/hotels/models/hotel.dart
+++ lib/Profiles/hotels/models/hotel.dart
@@ -1,12 +1,14 @@
 class Hotel {
-  final String name;
-  final String address;
-  final String phoneNumber;
-  final String description;
-  final String email;
-  final int ownerId;
+  final int? id;
+  final String? name;
+  final String? address;
+  final int? phoneNumber;
+  final String? description;
+  final String? email;
+  final int? ownerId;
 
   Hotel({
+    required this.id,
     required this.name,
     required this.address,
     required this.phoneNumber,
@@ -14,4 +16,16 @@ class Hotel {
     required this.description,
     required this.ownerId
   });
+
+  factory Hotel.fromJson(Map<String, dynamic> json) {
+    return Hotel(
+      id: json['id'] as int,
+      name: json['name'] as String,
+      address: json['address'] as String,
+      ownerId: json['ownersId'] as int,
+      description: json['description'] as String,
+      phoneNumber: json['phone'] as int,
+      email: json['email'] as String
+    );
+  }
 }
